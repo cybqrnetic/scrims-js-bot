1fc7e4bc2851bf4f617bca69e2fce7c7be05c539
aMeow <152869470+aMeow69@users.noreply.github.com>
2024-05-17 12:47:57 +0200

    nitro boost message

---
+++ src/modules/nitro-boost-handler.ts.ts
@@ -0,0 +1,24 @@
+import { Events, EmbedBuilder } from "discord.js"
+import { BotListener, Config } from "lib"
+
+const BOOST_CHANNEL = Config.declareType("Server Boost Channel")
+
+BotListener(Events.GuildMemberUpdate, async (bot, oldMember, newMember) => {
+    if (oldMember.premiumSince || !newMember.premiumSince) return
+
+    const boostChannelId = bot.getConfigValue(BOOST_CHANNEL, newMember.guild.id)
+    if (!boostChannelId) return
+
+    const boostChannel = await newMember.guild.channels.fetch(boostChannelId)
+    if (!boostChannel || !boostChannel.isTextBased()) return
+
+    const boostMessage = await boostChannel.send({
+        embeds: [
+            new EmbedBuilder()
+                .setTitle(`${newMember.user.username} has boosted the nitro!`)
+                .setDescription(`Thank you for boosting the nitro ${newMember}`)
+                .setThumbnail(newMember.user.displayAvatarURL()),
+        ],
+    })
+    await boostMessage.react("🎉")
+})
